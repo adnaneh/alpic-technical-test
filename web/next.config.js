6a3506ed35d741d177c8ddd3e784f766bac3fb7b
Adnane Hamid <adnane.hamid.20@gmail.com>
2025-08-21 19:21:31 +0200

    Update next.config.js

--- web/next.config.js
+++ web/next.config.js
@@ -1,14 +1,4 @@
-const API_BASE = process.env.NEXT_PUBLIC_API_BASE_URL || process.env.API_BASE_URL;
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    if (!API_BASE) return [];
-    return [
-      { source: "/api/:path*", destination: `${API_BASE.replace(/\/$/, "")}/api/:path*` },
-      { source: "/socket.io/:path*", destination: `${API_BASE.replace(/\/$/, "")}/socket.io/:path*` },
-    ];
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
